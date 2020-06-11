d583dced3ac77705b0fe2d02a3c577afc1113687
suba <subaudt27@gmail.com>
2020-06-11 12:34:18 +0530

    vowel counting

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var student = {
+//no of letters present in our name
+\var student = {
     name: "",
     type: "student"
   };
@@ -16,14 +17,21 @@ var student = {
   function calculateNumericOutput() {
     student.name = document.getElementById('name').value;
     var hey=0;
-    hey=student.name.length;
-    /*var totalNameValue = 0;
+    
+    var totalNameValue = 0;
     for (var i = 0; i < student.name.length; i++) {
       totalNameValue += student.name.charCodeAt(i);
     }
-  
+    var vowel=0;
+    for(var i=0;i<student.name.length;i++){
+        if(student.name.charAt(i)=='a' || student.name.charAt(i)=='e' || student.name.charAt(i)=='i' || student.name.charAt(i)=='o' || student.name.charAt(i)=='u' ){
+          vowel=vowel+1;
+        }
+    }
     // Insert result into page
-    //var out=student.name.length;*/
-    var output = "Total Numeric value of person's name is " + hey;
+    
+    var output = "Total Number of letters " + hey;
+    var result="Total number of vowels "+vowel;
     document.getElementById('output').innerText = output;
+    document.getElementById('result').innerText=result;
   }
