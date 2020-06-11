46d4f261d8858172c97113a69e8d61783386c5e1
suba <subaudt27@gmail.com>
2020-06-11 09:14:30 +0530

    count no of letters

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ var student = {
   
   function calculateNumericOutput() {
     student.name = document.getElementById('name').value;
-  
-    var totalNameValue = 0;
+    var output=student.name.length();
+    /*var totalNameValue = 0;
     for (var i = 0; i < student.name.length; i++) {
       totalNameValue += student.name.charCodeAt(i);
     }
   
     // Insert result into page
     //var out=student.name.length;
-    var output = "Total Numeric value of person's name is " + totalNameValue;
+    var output = "Total Numeric value of person's name is " + totalNameValue;*/
     document.getElementById('output').innerText = output;
   }
